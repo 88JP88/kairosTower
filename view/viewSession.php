532d558d62a32fe9b167adc1b1aeb148dd415048
Juan <juan.paez@api-ux.com>
2023-10-08 10:20:51 -0500

    pr base de prueba

--- view/viewSession.php
+++ view/viewSession.php
@@ -5,4 +5,5 @@ require_once 'layout/headerSession.php';
 require_once 'layout/sections.php';
 require_once 'modal/modalMyProfile.php';
 require_once 'modal/modalCloseSession.php';
+require_once 'modal/modalCloseSessionForce.php';
 ?>
