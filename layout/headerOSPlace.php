2f0b4ebb5017ff08c86855d2392a9814f274ef7b
88JP88 <gitjuan9@gmail.com>
2024-06-08 13:20:27 -0500

    commit

--- layout/headerOSPlace.php
+++ layout/headerOSPlace.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
   
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script src="libs/pako/dist/pako.min.js"></script>
+   
   </head>
   <body>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
